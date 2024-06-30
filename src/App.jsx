74afdb720dec8e6c9b0b3496a7752a0e43b2d937
attacker frnd <warriorsshadow39@gmail.com>
2024-06-30 10:30:09 +0530

    Adding Framer motion effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import Block from './Component/Reusable/Block'
+import React, { useRef } from 'react';
+import Block from './Component/Reusable/Block';
 
 const App = () => {
+  const containerRef = useRef(null);
+
   return (
-    <div className='h-[100%] bg-black text-white flex justify-center'>
-      <Block 
-        props='C++'
-      />
+    <div ref={containerRef} className='h-screen w-screen bg-black text-white flex justify-center items-center relative'>
+      <Block props='C++' containerRef={containerRef} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Component/Reusable/Block.jsx
+++ src/Component/Reusable/Block.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
+"use client"
+import React from 'react';
+import { motion } from 'framer-motion';
 
-const Block = ({props,src}) => {
+const Block = ({ props, src, containerRef }) => {
   return (
-    <div className='bg-slate-900 flex h-[50px] w-[180px] justify-center items-center border-[1px] border-sky-200 hover:border-purple-600 '>
-         <div>
-          <img src={src} alt="" />
-         </div>
-         <button className='text-white'>
-          {props}
-         </button>
-    </div>
-  )
+    <motion.div
+      className='bg-slate-900 flex h-[50px] w-[180px] justify-center items-center border-[1px] border-sky-200 hover:bg-black transition-all'
+      whileHover={{
+        background: 'black',
+        borderImage: 'linear-gradient(to right, #9f7aea, #6b46c1) 1'
+      }}
+      drag
+      dragConstraints={containerRef}
+      dragElastic={0.2}
+      style={{ position: 'absolute' }}
+      whileDrag={{scale:1.2}} 
+    >
+      <div>
+        {src && <img src={src} alt="" />}
+      </div>
+      <button className='text-white'>
+        {props}
+      </button>
+    </motion.div>
+  );
 }
 
-export default Block
+export default Block;
