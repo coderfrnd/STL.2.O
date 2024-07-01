7f189d608dc9c6dff0bdc87976e51ebf21badbf0
attacker frnd <warriorsshadow39@gmail.com>
2024-07-01 10:22:23 +0530

    Adding Particle.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import React, { useRef } from 'react';
 import Block from './Component/Reusable/Block';
+import ParticlesComponent from './Component/Particle/particle';
+// import ParticlesComponent from './Component/Particle/particle';
+import './App.css'
 
 const App = () => {
   const containerRef = useRef(null);
 
   return (
     <div ref={containerRef} className='h-screen w-screen bg-black text-white flex justify-center items-center relative'>
-      <Block props='C++' containerRef={containerRef} />
+      {/* <Block props='C++' containerRef={containerRef} /> */}
+     <div className='h-[100%]'>
+     <ParticlesComponent>
+     
+      </ParticlesComponent>
+     </div>
+     <div>
+      {/* <Block/> */}
+     </div>
+     <Block props='C++' containerRef={containerRef} />
+     <Block props='JAVA' containerRef={containerRef} />
+     <Block props='PYTHON' containerRef={containerRef} />
     </div>
   );
 }
